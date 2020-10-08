40ba749e0acf7dd9f4ba1b166899a6d663edaa53
brianwlego <legobw@gmail.com>
2020-10-08 14:12:00 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,34 @@
 // TODO: Create action creators as defined in tests
+
+const addQuote = (quote) => {
+  return {
+    type: 'ADD_QUOTE',
+    quote: quote
+  }
+}
+
+const removeQuote = (quoteId) => {
+  // console.log("inside remove quote action")
+  return {
+    type: 'REMOVE_QUOTE',
+    quoteId
+  }
+}
+
+const upvoteQuote = (quoteId) => {
+  // console.log("inside upvote action")
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteId
+  }
+}
+
+const downvoteQuote = (quoteId) => {
+  // console.log("inside downvote action", quoteId)
+  return {
+    type: 'DOWNVOTE_QUOTE',
+    quoteId
+  }
+}
+
+export { addQuote, removeQuote, upvoteQuote, downvoteQuote }
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,33 @@
 export default (state = [], action) => {
-  return state;
+  let idx;
+  let quote;
+  switch (action.type){
+    case "ADD_QUOTE":
+      console.log('add quote reducer')
+      return [...state, action.quote];
+
+    case "REMOVE_QUOTE":
+      console.log("inside reducer for remove quote")
+      idx = state.findIndex(quote => quote.id  === action.quoteId)
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+      
+    case "UPVOTE_QUOTE":
+      console.log("inside reducer for upvote quote")
+      quote = state.find(quote => quote.id === action.quoteId)
+      console.log(quote)
+      quote.votes += 1
+      return state
+
+    case "DOWNVOTE_QUOTE":
+      console.log("inside reducer for downvote quote")
+
+      quote = state.find(quote => quote.id === action.quoteId)
+      if (quote.votes > 0){
+        quote.votes -= 1
+      }
+      return state
+
+    default: 
+      return state
+  }
 }
